6f944ca930c7ec671d07bce969c47ed4f21eb189
Md.Habibulla <mdhabibulla146425@gmail.com>
2025-10-24 19:08:35 +0600

    layout visibal

---
+++ src/providers/layout-visibility-wraper.tsx
@@ -0,0 +1,42 @@
+
+'use client'
+import Hero from "@/components/shared/hero/Hero";
+import Navbar from "@/components/shared/navbar/Navbar";
+import { usePathname } from "next/navigation";
+import React from "react";
+
+const HIDDEN_ROUTES = [
+  "/signup",
+  "/login",
+  "/forgot-password",
+  "/reset-password",
+  "/verify",
+  "/verify-otp",
+  "/dashboard",
+  "/signinaspage",
+  "/forget-otp",
+  "/forget-password",
+  "/account/profile",
+  "/account/change-password",
+  "/account/booking-history",
+  "/account/tour-history",
+];
+
+interface LayoutVisibilityWrapperProps {
+  children: React.ReactNode;
+}
+
+const LayoutVisibilityWrapper: React.FC<LayoutVisibilityWrapperProps> = ({ children }) => {
+  const pathname = usePathname();
+  const shouldHideLayout = HIDDEN_ROUTES.some((route) => pathname.startsWith(route));
+
+  return (
+    <>
+      {!shouldHideLayout && <Navbar />}
+      {!shouldHideLayout && <Hero />}
+      {children}
+    </>
+  );
+};
+
+export default LayoutVisibilityWrapper;
